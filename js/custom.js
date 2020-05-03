24eea663b42a72f812d7c6572a0b087b858427ad
Vladimir Ilinskiy <creavit@i.ua>
2020-05-03 18:40:00 +0300

    final2

--- js/custom.js
+++ js/custom.js
@@ -6,9 +6,27 @@ function tilWhileNot(a){
 	регулировать частоту выдачи функцией значения truse,
 	задавая значения от 0 до 100 (процент распределения вероятности).
 	*/
-	if(a == 'unefined') a = 97;
+	if(a === undefined) a = 97;
 	a = a/100;
 	return Math.random() >= a;
 }
-var out = document.getElementById('out');
-out.innerText = tilWhileNot();
+// end of function tilWhileNot ----------------------
+
+var outf = document.getElementById('outf');
+var outt = document.getElementById('outt');
+research();
+function research(){
+	var inp = parseInt(document.getElementById('itr').value);
+	var f = 0;
+	var t = 0;
+	var i = 0;
+	var til = false;
+	do{
+		til = tilWhileNot();
+		f += til ? 0 : 1;
+		t += til ? 1 : 0;
+		i++;
+	}while(i < inp);
+	outf.innerText = 'Значений false: ' + Math.round(f*100/inp) + '%';
+	outt.innerText = 'Значений true: ' + Math.round(t*100/inp) + '%';
+}
